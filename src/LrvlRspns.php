46f5e0170d25a8bd8321d1dc8590aaf739a67fa9
Coen Onderwater <coenonderwater@gmail.com>
2018-12-09 20:53:00 +0100

    feature: try new way of response functions

--- src/LrvlRspns.php
+++ src/LrvlRspns.php
@@ -2,15 +2,16 @@
 
 namespace coenond\LrvlRspns;
 
-use Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 
-class LrvlRspns
+class LrvlRspnss
 {
 	/**
 	 * @param $data  		Array 	| optional, Resource(s) to return
 	 * @param $statusCode 	int 	| optional, 200 default
 	 */
-	function rspnsSuccess($data = [], $statusCode = 200): Response
+	function rspnsSuccess($data = [], $statusCode = 200): JsonResponse
 	{
 		$response = [
 			"status" => "success",
@@ -24,7 +25,7 @@ class LrvlRspns
 	 * @param $errorMsg 	String 	| optional
 	 * @param $statusCode 	int 	| optional, 500 default
 	 */
-	function rspnsError($errorMsg = "Oops, something went wrong!", $statusCode = 500): Response
+	function rspnsError($errorMsg = "Oops, something went wrong!", $statusCode = 500): JsonResponse
 	{
 		$response = [
 			"status" => "error",
@@ -38,7 +39,7 @@ class LrvlRspns
 	 * @param $data  		Array 	| Error messages to return
 	 * @param $statusCode 	int 	| optional, 400 default
 	 */
-	function rspnsFail($data = [], $statusCode = 400): Response
+	function rspnsFail($data = [], $statusCode = 400): JsonResponse
 	{
 		$response = [
 			"status" => "fail",
@@ -46,5 +47,89 @@ class LrvlRspns
 		];
 
 		return response()->json($data, $statusCode);
+	}
+
+	/**
+	 * Response OK 200
+	 * @param  array  $data 
+	 */
+	function ok($data = []): JsonResponse
+	{
+		$response = [
+			"status" => "success",
+			"data" => $data
+		];
+
+		return response()->json($data, Response::HTTP_OK);
+	}
+
+	/**
+	 * Response CREATED 201
+	 * @param  array  $data 
+	 */
+	function created($data = []): JsonResponse
+	{
+		$response = [
+			"status" => "success",
+			"data" => $data
+		];
+
+		return response()->json($data, Response::HTTP_CREATED);
+	}
+
+	/**
+	 * Response ACCEPTED 202
+	 * @param  array  $data 
+	 */
+	function accepted($data = []): JsonResponse
+	{
+		$response = [
+			"status" => "success",
+			"data" => $data
+		];
+
+		return response()->json($data, Response::HTTP_ACCEPTED);
+	}
+
+	/**
+	 * Response NO_CONTENT 204
+	 * @param  array  $data 
+	 */
+	function noContent($data = []): JsonResponse
+	{
+		$response = [
+			"status" => "success",
+			"data" => $data
+		];
+
+		return response()->json($data, Response::HTTP_NO_CONTENT);
+	}
+
+	/**
+	 * Response BAD_REQUEST 400
+	 * @param  array  $data 
+	 */
+	function badRequest($data = []): JsonResponse
+	{
+		$response = [
+			"status" => "error",
+			"data" => $data
+		];
+
+		return response()->json($data, Response::HTTP_BAD_REQUEST);
+	}
+
+	/**
+	 * Response UNAUTHORIZED 401
+	 * @param  array  $data 
+	 */
+	function unauthorized($data = []): JsonResponse
+	{
+		$response = [
+			"status" => "error",
+			"data" => $data
+		];
+
+		return response()->json($data, Response::HTTP_UNAUTHORIZED);
 	}
 }
