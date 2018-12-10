c5cfb08b65ddd549de9789e137d541f2ba6f46f1
Coen Onderwater <coenonderwater@gmail.com>
2018-12-10 11:56:13 +0100

    refactor: remove depricated functions

--- src/LrvlRspns.php
+++ src/LrvlRspns.php
@@ -8,48 +8,6 @@ use Illuminate\Http\Response;
 class LrvlRspnss
 {
 	/**
-	 * @param $data  		Array 	| optional, Resource(s) to return
-	 * @param $statusCode 	int 	| optional, 200 default
-	 */
-	function rspnsSuccess($data = [], $statusCode = 200): JsonResponse
-	{
-		$response = [
-			"status" => "success",
-			"data" => $data ?: []
-		];
-		
-		return response()->json($response, $statusCode);
-	}
-
-	/**
-	 * @param $errorMsg 	String 	| optional
-	 * @param $statusCode 	int 	| optional, 500 default
-	 */
-	function rspnsError($errorMsg = "Oops, something went wrong!", $statusCode = 500): JsonResponse
-	{
-		$response = [
-			"status" => "error",
-			"message" => $errorMsg
-		];
-
-		return response()->json($response, $statusCode);
-	}
-
-	/**
-	 * @param $data  		Array 	| Error messages to return
-	 * @param $statusCode 	int 	| optional, 400 default
-	 */
-	function rspnsFail($data = [], $statusCode = 400): JsonResponse
-	{
-		$response = [
-			"status" => "fail",
-			"data" => $data
-		];
-
-		return response()->json($data, $statusCode);
-	}
-
-	/**
 	 * Response OK 200
 	 * @param  array  $data 
 	 */
